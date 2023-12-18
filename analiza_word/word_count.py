21dec8fff9e4dffe8462142d5142da80a61ea024
malgosiam2 <126112315+malgosiam2@users.noreply.github.com>
2023-12-18 18:44:06 +0100

    word analysis

--- analiza_word/word_count.py
+++ analiza_word/word_count.py
@@ -1,10 +1,8 @@
+import pathlib
 from datetime import datetime
-from pathlib import Path
-
 import docx2txt
 import re
 import os
-
 import pandas as pd
 
 
@@ -26,20 +24,24 @@ def count_przecinki(docx_path):
     result = docx2txt.process(docx_path)
     return result.count(",")
 
+
 # zliczanie wszystkich wykrzyknikow
 def count_wykrzyknik(docx_path):
     result = docx2txt.process(docx_path)
     return result.count("!")
+
 
 # zliczanie wszystkich znkaow zapytania
 def count_pytajnik(docx_path):
     result = docx2txt.process(docx_path)
     return result.count("?")
 
+
 # zliczanie wszystkich myslnikow
 def count_myslnik(docx_path):
     result = docx2txt.process(docx_path)
     return result.count("-")
+
 
 # zliczanie wszystkich dwukropkow
 def count_dwukropek(docx_path):
@@ -57,13 +59,12 @@ def count_others(docx_path):
 def zlozonosc_zdan(docx_path):
     result = docx2txt.process(docx_path)
     wielokropek = result.count("...")
-    liczba_zdan = result.count(".") - 2 * wielokropek
+    liczba_zdan = result.count(".") - 2 * wielokropek - count_pytajnik(docx_path) - count_wykrzyknik(docx_path)
     if liczba_zdan == 0:
         return None
     return count_przecinki(docx_path) / liczba_zdan
 
 
-# znajdowanie najdłuższego zdania w tekscie -> mało sensu, ponieważ nie wszystkie pliki są w zdaniach
 def znajdz_najdluzsze_zdanie(docx_path):
     result = docx2txt.process(docx_path)
     lista_zdan = re.split(r'[.!?]', result)
@@ -75,40 +76,45 @@ def znajdz_najdluzsze_zdanie(docx_path):
 def znajdz_najdluzsze_slowo(docx_path):
     result = docx2txt.process(docx_path)
     lista = result.split()
-    lista = [s.replace(',', '').replace('\n', '').replace(".", "").replace(":", "") for s in lista]
+    lista = [s.replace(',', '').replace('\n', '').replace(".", "").replace(":", "").replace("\"", "") for s in lista]
     return max(lista, key=len)
 
 
-def main():
-    #od razu tworzę ramkę danych z wszystkimi potrzebnymi kolumnami i zapisuję do pliku csv
+def data_utworzenia_pliku(docx_path):
+    if os.path.exists(docx_path):
+        timestamp_utworzenia = os.path.getctime(docx_path)
+        data_utworzenia = datetime.fromtimestamp(timestamp_utworzenia)
+        return data_utworzenia.strftime('%Y-%m-%d')
+    else:
+        return None
 
-    folder_path = './docx/'
+
+def main():
+    folder_path = "C:\\Users\\HP\\PycharmProjects\\skrypty\\word_pliki"
+    sciezki_do_plikow = list(pathlib.Path(folder_path).rglob('*.docx'))
+
+    imie = "Malgosia"
     data = []
 
     if os.path.exists(folder_path) and os.path.isdir(folder_path):
-        files = os.listdir(folder_path)
-        for el in files:
-            #file_stat = Path(folder_path + el).stat()
-            modify_time = os.path.getmtime(folder_path + el)
-            modify_time = datetime.fromtimestamp(modify_time)
-            #creation_time = file_stat.st_ctime
-            #creation_date = datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d')
-            data.append({"Imię": "Malgosia",
-                         "Data utworzenia": modify_time,
+        for el in sciezki_do_plikow:
+            data.append({"Imie": imie,
+                         "Data utworzenia pliku": data_utworzenia_pliku(el),
                          "Rozszerzenie": "docx",
-                         "Nazwa pliku": el,
-                         "Ilość słów:": count_words(folder_path + el),
-                         "Ilość przecinków": count_przecinki(folder_path + el),
-                         "Ilość pytajnikow": count_pytajnik(folder_path + el),
-                         "Ilość wykrzyknikow": count_wykrzyknik(folder_path + el),
-                         "Ilość kropek": count_dots(folder_path + el),
-                         "Ilość myslnikow": count_myslnik(folder_path + el),
-                         "Ilość dwukropkow": count_dwukropek(folder_path + el),
-                         "Ilość pozostałych znaków": count_others(folder_path + el),
-                         "Złożoność zdań": zlozonosc_zdan(folder_path + el),
-                        "Najdłuższe słowo": znajdz_najdluzsze_slowo(folder_path + el)})
+                         "Nazwa pliku": el.name,
+                         "Ilosc słow:": count_words(el),
+                         "Ilosc przecinkow": count_przecinki(el),
+                         "Ilosc pytajnikow": count_pytajnik(el),
+                         "Ilosc wykrzyknikow": count_wykrzyknik(el),
+                         "Ilosc kropek": count_dots(el),
+                         "Ilosc myslnikow": count_myslnik(el),
+                         "Ilosc dwukropkow": count_dwukropek(el),
+                         "Ilosc pozostalych znakow": count_others(el),
+                         "Zlozonosc zdan": zlozonosc_zdan(el),
+                         "Najdluzsze slowo": znajdz_najdluzsze_slowo(el)})
     df = pd.DataFrame(data)
-    # df.to_csv("aaa")
+    # print(df)
+    df.to_csv("Malgosia-word.csv")
 
 
 if __name__ == "__main__":
