3f2398bb73533fc94f5414f9e36eb8f530978b78
MikolajRowicki <01180738@pw.edu.pl>
2023-12-20 11:43:36 +0100

    matlab check

--- Mikolaj_python_code/data/func.m
+++
@@ -1,4 +0,0 @@
-function [y] = func(x)
-%FUNC oblicza wartość funkcji trygonometrycznej
-y = 2 * cos(x/2);
-end
--- Mikolaj_python_code/data/pochodnaD.m
+++
@@ -1,4 +0,0 @@
-function [fa] = pochodnaD(x)
-%POCHODNAD oblicza dokładną wartość pochodnej funkcji f
-fa = -sin(x/2);
-end
